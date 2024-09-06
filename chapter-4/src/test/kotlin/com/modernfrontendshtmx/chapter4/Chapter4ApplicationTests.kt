dc436042eb010d9636fe29a72213d76e1837e641
Sebastián Muñoz <cloud+github@electronmail.info>
2024-09-06 10:40:51 -0400

    master frontend chapter 3 end, chapter 4 init

---
+++ chapter-4/src/test/kotlin/com/modernfrontendshtmx/chapter4/Chapter4ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.modernfrontendshtmx.chapter4
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter4ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
